2fa33cd49dff8b1f7c4fe49a6ac83b70bde67a6a
Zan Ahmad <60706569+zanahmad@users.noreply.github.com>
2023-05-27 17:32:32 -0400

    added all cases for plots

--- steady_state/fix_F_vary_compliances_g_tol.py
+++ steady_state/fix_F_vary_compliances_g_tol.py
@@ -1,81 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set_theme()
-from matplotlib.ticker import FormatStrFormatter
-
 from parameters import *
 
-# pressures
-P_thorax = -4*1333
-P_RA = P_thorax + dP_RA
+# Fix P_thorax to a single value
+P_thorax = -4 * 1333
 
-# for a given hypothetical maximum HR
-max_F = 180 # maximum allowed heart rate
-
-# discretization
+# Discretization
 dx = 100
-Csa_u_range = np.linspace(0.5*Csa_u, 1.5*Csa_u, dx)
-Csa_l_range = np.linspace(0.5*Csa_l, 1.5*Csa_l, dx)
+Csa_u = np.linspace(0.5 * Csa_u, 1.5 * Csa_u, dx)
+Csa_l = np.linspace(0.5 * Csa_l, 1.5 * Csa_l, dx)
 
-gtol_vs_CsaU_vs_CsaL= np.zeros((len(Csa_u_range), len(Csa_l_range)))
-
-F = max_F
 Hu = Hu_patient
 Hl = Hl_patient
 
-for i in range(dx):
-    for j in range(dx):
-            # Vd_total = Vtotal - Cp * (C_RVD / C_LVD) * (dP_RA) - ...
-            #           (Tp * Gs + Csa) * Psa_u_star - (Tp * Gs_l + Csa_l) ...
-            #           * rho * g_earth * Hu - Cs_l * rho * g_earth * (- Hl)
-            # Setting Vd_total to zero to find g tol
+# Initialize arrays to store G tolerance values
+G_tolerance = np.zeros((len(Csa_u), len(Csa_l)))
 
-            Csa_u = Csa_u_range[i]
-            Csa_l = Csa_l_range[j]
-            Csa = Csa_l + Csa_u
-            Cs_l = Csa_l + Csv_l
-            g_tol = (Vtotal - Cp*(C_RVD/C_LVD)*(dP_RA) -
-                                (Tp*Gs+Csa)*Psa_u_star)/((Tp*Gs_l+ Csa_l)
-                                                         *rho*Hu-Cs_l*rho*Hl)
-            gtol_vs_CsaU_vs_CsaL[i,j] = g_tol
-            # Q = ((1 / Rs_u) + (1 / Rs_l)) * Psa_u_star + rho * g_tol * Hu / Rs_l
-            # Ppv = P_thorax + (C_LVD / C_RVD) * dP_RA
-            # Ppa = Ppv + Q * Rp
-        # cases[j, i] = 1
+# Loop over Csa_u and Csa_l values
+for i in range(len(Csa_u)):
+    for j in range(len(Csa_l)):
+        G = np.linspace(g_earth, 10 * 980, 3000)
 
-# conversions:
-gtol_vs_CsaU_vs_CsaL = gtol_vs_CsaU_vs_CsaL / 100 / (g_earth / 100)
-Csa_u_range = np.around(Csa_u_range, 6) # ml/mmHg
-Csa_l_range = np.around(Csa_l_range, 6) # ml/mmHg
+        # Initialize Vd_total vector
+        Vd_total_vec = np.empty(len(G))
 
-# heatmap plot
+        for k in range(len(G)):
+            if P_thorax <= -dP_RA:
+                Vd_total = Vtotal - Cp * (C_RVD / C_LVD) * (dP_RA) \
+                           - (Tp * Gs + Csa_u[i]+Csa_l[j]) * Psa_u_star \
+                           - (Tp * Gs_l + Csa_l[j]) * rho * G[k] * Hu - (Csa_l[j] +Csv_l) * rho * G[k] * (-Hl)
+            elif P_thorax > -dP_RA and P_thorax < rho * G[k] * Hu - dP_RA:
+                Vd_total = Vtotal - Cp * (C_RVD / C_LVD) * dP_RA \
+                           - (Tp * Gs + Csa_u[i]+Csa_l[j]) * Psa_u_star \
+                           - (Tp * Gs_l + Csa_l[j]) * (rho * G[k] * Hu) \
+                           - Cs_l * rho * G[k] * (-Hl) \
+                           - (Csv_l - Tp * Gs_l) * (P_thorax + dP_RA)
+            elif P_thorax >= rho * G[k] * Hu - dP_RA:
+                Vd_total = Vtotal - Cp * (C_RVD / C_LVD) * dP_RA \
+                           - (Tp * Gs + Csa_l[j] + Csa_u[i]) * Psa_u_star \
+                           - (Tp * Gs_l + Csa_l[j] - Csv_u) * rho * G[k] * Hu \
+                           - (Csa_l[j] + Csv_l) * rho * G[k] * (-Hl) \
+                           - (Csv_l + Csv_u - Tp * Gs) * (P_thorax + dP_RA)
 
-num_ticks = 10
-# the index of the position of yticks
-yticks_ = np.linspace(0, dx - 1, num_ticks, dtype=int)
-# the content of labels of these yticks
-yticklabels_ = [str(Csa_u_range[idx]) for idx in yticks_]
+            if Vd_total >= 0:
+                Vd_total_vec[k] = Vd_total
+            else:
+                Vd_total_vec[k] = float('nan')
 
-xticks_ = np.linspace(0, dx - 1, num_ticks, dtype=int)
-# the content of labels of these yticks
-xticklabels_ = [str(Csa_l_range[idx]) for idx in xticks_]
+        # Find G tolerance value
+        zero_indices = np.where(Vd_total_vec <= 0)[0]
+        if len(zero_indices) > 0:
+            G_tolerance[i, j] = G[zero_indices[0]] / 1000
+        else:
+            G_tolerance[i, j] = G[np.argmin(np.abs(Vd_total_vec))] / 1000
 
+# Plotting the heatmap
 fig, ax = plt.subplots()
-im = ax.imshow(gtol_vs_CsaU_vs_CsaL)
-fig.colorbar(im, ax=ax)
-# ax = sns.heatmap(gtol_vs_CsaU_vs_CsaL)
+heatmap = ax.imshow(G_tolerance, cmap='jet', aspect='auto', origin='lower')
 
-# Show all ticks and label them with the respective list entries
-ax.set_xticks(xticks_, labels=xticklabels_)
-ax.set_yticks(yticks_, labels=yticklabels_)
-# ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-# ax.xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-ax.grid(False)
-# Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-fig.tight_layout(pad=2  )
-
-plt.ylabel('upper compliance', fontsize = 12)
-plt.xlabel('lower compliance', fontsize = 12)
-plt.savefig('compliance_gtol_height_factor.png')
+ax.set_xlabel('Csa_l (ml/mmHg)')
+ax.set_ylabel('Csa_u (ml/mmHg)')
+ax.set_title('G Tolerance Heatmap')
+plt.colorbar(heatmap, label='G Multiples')
+plt.grid(True)
+plt.show()
