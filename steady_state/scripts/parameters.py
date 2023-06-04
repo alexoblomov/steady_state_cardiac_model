7f3e438f32bc95695cc077afbd615668d3905609
alanna kennard <alanna.kennard@smartmeup.io>
2023-06-04 19:05:45 +0200

    bugfix revert parameters to conference values

--- steady_state/scripts/parameters.py
+++ steady_state/scripts/parameters.py
@@ -12,17 +12,22 @@ Ts, the time constant is in seconds
 density is in g/cm3
 """
 color_map = 'plasma' #initialize MPL color map for uniform results
+
 Psa_u_star = 100 * 1333
 
 Psa_u = Psa_u_star
 dP_RA = 2 * 1333
+height = 167.64
 
-Hu_patient = 32
-Hl_patient = -42
+# 1/2 factor to average over compartment height (bernoulli's principle - 
+# fluid dynamics)
+
+Hu_patient = 0.5*32 # (heart 2 eyeball)
+Hl_patient = -0.5*42 # (heart 2 seat)
 lumped_height = Hu_patient + (-Hl_patient)
 
-Hu_factor = Hu_patient /lumped_height
-Hl_factor = - Hl_patient/lumped_height
+Hu_factor = 1/3
+Hl_factor = 2/3
 
 rho = 1
 
@@ -41,8 +46,10 @@ C_RVD = (0.035 / 1333) * 1000
 
 C_LVD = (0.00583 / 1333) * 1000
 
-Csa_l = Hl_factor * (0.00175 / 1333) * 1000 
-Csa_u = Hu_factor * (0.00175 / 1333) * 1000 
+# ideally we want to remove h factor from compliances
+# -- but g tol will decrease once Cs_i increase
+Csa_l = Hl_factor * (0.00175 / 1333) * 1000
+Csa_u = Hu_factor * (0.00175 / 1333) * 1000
 Csv_l = Hl_factor * (0.09 / 1333) * 1000
 Csv_u = Hu_factor * (0.09 / 1333) * 1000
 Cs_l = Csa_l + Csv_l
@@ -54,5 +61,4 @@ Vtotal = 3.7 * 1000
 Csa = Csa_l + Csa_u
 # Gs = 1 / Rs_u + 1 / Rs_l
 Ts = Csa_u * Rs_u
-
 Tp = Rp * Cpa
