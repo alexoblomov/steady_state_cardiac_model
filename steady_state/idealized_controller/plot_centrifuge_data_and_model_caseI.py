a95471f010d7198c33d8c37f16a120946dbd3970
alanna kennard <alanna.kennard@smartmeup.io>
2024-11-24 19:19:12 +0100

    working superposition plot for model and centrifuge data

--- steady_state/idealized_controller/plot_centrifuge_data_and_model_caseI.py
+++ steady_state/idealized_controller/plot_centrifuge_data_and_model_caseI.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from parameters import *
 from matplotlib import rc
 import matplotlib as mpl
+from matplotlib.lines import Line2D
+
 mpl.rcParams['mathtext.fontset'] = 'cm'
 
 from run_model import plot_F_vs_G_case_I
@@ -14,19 +16,17 @@ from plot_centrifuge_data import plot_centrifuge_data
 
 def main():
     fig, ax = plt.subplots()
-    # plt.axis((0, 4, 40, 200))
-    # plot_F_vs_G_case_I()
+
     gz_list, hr_list = plot_centrifuge_data()
     G, sol_F_Vtotal_G = plot_F_vs_G_case_I()
-    plt.plot(G, sol_F_Vtotal_G)
-    plt.plot(gz_list, hr_list, 'ro')
+
+    plt.plot(G, sol_F_Vtotal_G, label="model prediction")
+    plt.plot(gz_list, hr_list, 'ro', label="data points")
     
-    # breakpoint()
     plt.axis((0, 4, 40, 200))
+    plt.legend(loc="upper right")
 
     plt.savefig("combined_data.png")
-
-    # breakpoint()
 
 if __name__ == "__main__":
     main()
