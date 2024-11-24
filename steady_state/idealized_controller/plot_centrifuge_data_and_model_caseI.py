a9ec806bda0d9299f9ea9fdbe9d6f5a1eb5bc54d
alanna kennard <alanna.kennard@smartmeup.io>
2024-11-24 19:28:48 +0100

    changed dot format

--- steady_state/idealized_controller/plot_centrifuge_data_and_model_caseI.py
+++ steady_state/idealized_controller/plot_centrifuge_data_and_model_caseI.py
@@ -20,8 +20,8 @@ def main():
     gz_list, hr_list = plot_centrifuge_data()
     G, sol_F_Vtotal_G = plot_F_vs_G_case_I()
 
-    plt.plot(G, sol_F_Vtotal_G, label="model prediction")
-    plt.plot(gz_list, hr_list, 'ro', label="data points")
+    plt.plot(G, sol_F_Vtotal_G, color = "b", label="model prediction")
+    plt.plot(gz_list, hr_list, 'r.', label="data points")
     
     plt.axis((0, 4, 40, 200))
     plt.legend(loc="upper right")
